f9f03221875619c8c3ae5a1e03c8edef51fd0014
Pavel Timofeev <paul.v.timofeev@gmail.com>
2020-12-23 00:34:24 +0300

    init

---
+++ src/test/java/com/patico/angulartest/AngulartestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patico.angulartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngulartestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
