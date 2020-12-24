104789984ab486c1926cbc57cef4c66e1a1cd213
Pavel Timofeev <paul.v.timofeev@gmail.com>
2020-12-25 02:22:06 +0300

    save...

--- src/main/java/com/patico/angulartest/AngulartestApplication.java
+++ src/main/java/com/patico/angulartest/AngulartestApplication.java
@@ -2,9 +2,16 @@ package com.patico.angulartest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+//import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -22,7 +29,30 @@ public class AngulartestApplication {
         return model;
     }
 
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+
+        return user;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AngulartestApplication.class, args);
+    }
+
+    @Configuration
+    @Order(SecurityProperties.IGNORED_ORDER)
+//    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http
+              .httpBasic()
+              .and()
+              .authorizeRequests()
+              .antMatchers("/index.html", "/", "/home", "/login").permitAll()
+              .anyRequest().authenticated();
+        }
     }
 }
